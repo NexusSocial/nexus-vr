4236b364ccdd39ccdcc8f9ca7016db6ea966cf3e
Ryan Butler <thebutlah@gmail.com>
2023-11-24 02:52:24 -0500

    Added clone assign benchmark

--- crates/universal-capture/benches/common_mem_ops.rs
+++ crates/universal-capture/benches/common_mem_ops.rs
@@ -34,16 +34,18 @@ fn random_frames(res: Dims, num_frames: usize) -> Vec<Vec<u8>> {
 	frames
 }
 
-fn bench_memcpy(c: &mut Criterion) {
-	let n_frames = 128;
-	let frames_1080 = random_frames(RES_1080, n_frames);
-	let frames_1440 = random_frames(RES_1440, n_frames);
+fn bench_frame_copy(c: &mut Criterion) {
+	/// Number of source frames we hold in memory as inputs.
+	const N_FRAMES_IN_MEMORY: usize = 128;
+
+	let frames_1080 = random_frames(RES_1080, N_FRAMES_IN_MEMORY);
+	let frames_1440 = random_frames(RES_1440, N_FRAMES_IN_MEMORY);
 	let mut dest_1080 = vec![0; RES_1080.size()];
 	let mut dest_1440 = vec![0; RES_1440.size()];
 
-	let mut group = c.benchmark_group("memcpy");
+	let mut group = c.benchmark_group("frame copy");
 	group.throughput(criterion::Throughput::Elements(
-		n_frames.try_into().unwrap(),
+		N_FRAMES_IN_MEMORY.try_into().unwrap(),
 	));
 
 	#[inline]
@@ -55,15 +57,42 @@ fn bench_memcpy(c: &mut Criterion) {
 		}
 	}
 
-	group.bench_function("memcpy 1080", |b| {
-		b.iter(|| memcpy_frames(frames_1080.as_slice(), &mut dest_1080))
+	#[inline]
+	fn clone_assign_frames(source: &[Vec<u8>], mut dest: &mut Vec<u8>) {
+		let source = black_box(source);
+		for f in source {
+			// This allocates and copies
+			let cloned_vec = f.clone();
+			// This deallocates the vec that used to be in `dest`
+			*dest = cloned_vec;
+			dest = black_box(dest);
+		}
+	}
+
+	group.bench_with_input(BenchmarkId::new("memcpy", "1080p"), &(), |b, &()| {
+		let source = frames_1080.as_slice();
+		let dest = dest_1080.as_mut_slice();
+		b.iter(|| memcpy_frames(source, dest))
 	});
-	group.bench_function("memcpy 1440", |b| {
-		b.iter(|| memcpy_frames(frames_1440.as_slice(), &mut dest_1440))
+	group.bench_with_input(BenchmarkId::new("memcpy", "1440p"), &(), |b, &()| {
+		let source = frames_1440.as_slice();
+		let dest = dest_1440.as_mut_slice();
+		b.iter(|| memcpy_frames(source, dest))
+	});
+
+	group.bench_with_input(BenchmarkId::new("clone assign", "1080p"), &(), |b, &()| {
+		let source = frames_1080.as_slice();
+		let dest = &mut dest_1080;
+		b.iter(|| clone_assign_frames(source, dest))
+	});
+	group.bench_with_input(BenchmarkId::new("clone assign", "1440p"), &(), |b, &()| {
+		let source = frames_1440.as_slice();
+		let dest = &mut dest_1440;
+		b.iter(|| clone_assign_frames(source, dest))
 	});
 
 	group.finish();
 }
 
-criterion_group!(benches, bench_memcpy);
+criterion_group!(benches, bench_frame_copy);
 criterion_main!(benches);

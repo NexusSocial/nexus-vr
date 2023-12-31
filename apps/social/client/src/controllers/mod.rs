01a8b780c035e1c4345f7a94f4c5ffe6ff1cc395
Malek <pocmalek@gmail.com>
2023-12-31 11:58:06 -0800

    drive poses with xr, or at least try (#59)

--- apps/social/client/src/controllers/mod.rs
+++ apps/social/client/src/controllers/mod.rs
@@ -6,6 +6,14 @@ use bevy::{
 	reflect::Reflect,
 	time::Time,
 };
+use bevy::ecs::query::QuerySingleError;
+use bevy::prelude::{GlobalTransform, Mut};
+use bevy_oxr::input::XrInput;
+use bevy_oxr::resources::XrFrameState;
+use bevy_oxr::xr_input::oculus_touch::OculusController;
+use bevy_oxr::xr_input::trackers::OpenXRTrackingRoot;
+use bevy_oxr::xr_input::{QuatConv, Vec3Conv};
+use social_networking::data_model::{Local, PlayerPose};
 
 use crate::pose_gizmo;
 
@@ -18,7 +26,8 @@ impl Plugin for KeyboardControllerPlugin {
 		app.register_type::<Direction>()
 			.init_resource::<Direction>()
 			.add_systems(PreUpdate, direction_from_keys)
-			.add_systems(PreUpdate, move_controlled_entities);
+			.add_systems(PreUpdate, move_controlled_entities)
+			.add_systems(PreUpdate, drive_input_pose);
 	}
 }
 
@@ -103,4 +112,63 @@ fn move_controlled_entities(
 		t.translation += parent_vel;
 		pose_gizmo(&mut gizmos, &t, Color::GRAY);
 	}
+}
+
+fn drive_input_pose(
+	oculus_controller: Option<Res<OculusController>>,
+	frame_state: Option<Res<XrFrameState>>,
+	xr_input: Option<Res<XrInput>>,
+	tracking_root: Query<&GlobalTransform, &OpenXRTrackingRoot>,
+	mut player_pose: Query<&mut social_networking::data_model::PlayerPose, With<Local>>
+) {
+	let mut player_pose = match player_pose.get_single_mut() {
+		Ok(player_pose) => player_pose,
+		Err(_) => return,
+	};
+	let mut tracking_root = match tracking_root.get_single() {
+		Ok(tracking_root) => tracking_root,
+		Err(_) => return,
+	};
+	let (oculus_controller, frame_state, xr_input) = match (oculus_controller, frame_state, xr_input) {
+		(Some(oculus_controller), Some(frame_state), Some(xr_input)) => (oculus_controller, frame_state, xr_input),
+		(_, _, _) => return,
+	};
+	let frame_state = *frame_state.lock().unwrap();
+	let grip_space = oculus_controller
+		.grip_space
+		.as_ref()
+		.expect("missing grip space on controller");
+
+	let right_controller = match grip_space
+		.right
+		.relate(&xr_input.stage, frame_state.predicted_display_time) {
+		Ok(r) => r,
+		Err(_) => return,
+	};
+	let left_controller = match grip_space
+		.left
+		.relate(&xr_input.stage, frame_state.predicted_display_time) {
+		Ok(l) => l,
+		Err(_) => return,
+	};
+
+	let headset_input = match xr_input
+		.head
+		.relate(&xr_input.stage, frame_state.predicted_display_time) {
+		Ok(h) => h,
+		Err(_) => return,
+	};
+
+	player_pose.root.trans = tracking_root.translation();
+	player_pose.root.rot = tracking_root.to_scale_rotation_translation().1;
+
+	player_pose.head.trans = headset_input.0.pose.position.to_vec3();
+	player_pose.head.rot = headset_input.0.pose.orientation.to_quat();
+
+	player_pose.hand_r.trans = right_controller.0.pose.position.to_vec3();
+	player_pose.hand_r.rot = right_controller.0.pose.orientation.to_quat();
+
+	player_pose.hand_l.trans = right_controller.0.pose.position.to_vec3();
+	player_pose.hand_l.rot = right_controller.0.pose.orientation.to_quat();
+
 }

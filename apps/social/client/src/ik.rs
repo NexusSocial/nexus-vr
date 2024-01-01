59d7d8918d53b7c75254096458a0694b9387513e
Schmarni <marnistromer@gmail.com>
2024-01-01 23:22:20 +0100

    fmt

--- apps/social/client/src/ik.rs
+++ apps/social/client/src/ik.rs
@@ -181,8 +181,10 @@ fn update_ik(
 			let flip_quat_r = Quat::from_rotation_z(-PI / 2.);
 			// At this point, the skeleton is known to be valid; next, handle VR input
 			let final_head = iso_to_transform(&pose.root.mul_isometry(&pose.head));
-			let final_left_hand = iso_to_transform(&pose.root.mul_isometry(&pose.hand_l));
-			let final_right_hand = iso_to_transform(&pose.root.mul_isometry(&pose.hand_r));
+			let final_left_hand =
+				iso_to_transform(&pose.root.mul_isometry(&pose.hand_l));
+			let final_right_hand =
+				iso_to_transform(&pose.root.mul_isometry(&pose.hand_r));
 			// now everything is set up and IK logic can begin.
 			let head_rot_euler = (skeleton_comp.root_defaults.head.rotation.inverse()
 				* final_head.rotation)
